a6f63067e9155aa1449f2703a0a7f7e15f639b93
Pranshu Singhal <GREYORANGE\pranshu.s@ggn001271.greyorange.com>
2019-07-24 12:33:24 +0530

    some info

--- portfolio/jobs/models.py
+++ portfolio/jobs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# models are python classes that can be saved inti the database
 class Job(models.Model):
 	image = models.ImageField(upload_to='images/')
 	summary = models.CharField(max_length=200)
